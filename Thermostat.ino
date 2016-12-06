eb6372a56ad4e9dbe0a5030d5a6c0c48970e1b92
Mike McRoberts <thearduinoguy@users.noreply.github.com>
2016-12-06 21:43:04 +0000

    Create Thermostat.ino

---
+++ Thermostat.ino
@@ -0,0 +1,97 @@
+
+int on[]   = {0, 0, 0, 0, 0, 0, 0, 1, 0, 2, 0, 0, 1, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1};
+
+int off[]  = {0, 0, 0, 0, 0, 0, 0, 1, 0, 2, 0, 0, 1, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1};
+
+/****************************************************************/
+
+void setup() {
+  pinMode(2, OUTPUT);
+  Serial.begin(115200);
+  Serial.println("Ready");
+  delay(2000);
+}
+
+/****************************************************************/
+
+void heatingOn()
+{
+
+  // Turn off the output.
+  Serial.println("Boiler On");
+  for (int repeat = 1; repeat <= 3; repeat++)
+  {
+    for (int i = 0; i < sizeof( on ) / sizeof( int ); i++) {
+      switch (on[i])
+      {
+        case 0:
+          digitalWrite(2, LOW);
+          delayMicroseconds(500);
+          digitalWrite(2, HIGH);
+          delayMicroseconds(500);
+          break;
+        case 1:
+          digitalWrite(2, HIGH);
+          delayMicroseconds(500);
+          digitalWrite(2, LOW);
+          delayMicroseconds(500);
+          break;
+        case 2:
+          digitalWrite(2, LOW);
+          delayMicroseconds(500);
+          break;
+      }
+
+    }
+    delay(1000);
+
+  }
+
+}
+
+/****************************************************************/
+
+void heatingOff()
+{
+
+
+  Serial.println("Boiler Off");
+  for (int repeat = 1; repeat <= 3; repeat++)
+  {
+    for (int i = 0; i < sizeof( off ) / sizeof( int ); i++) {
+      switch (off[i])
+      {
+        case 0:
+          digitalWrite(2, LOW);
+          delayMicroseconds(500);
+          digitalWrite(2, HIGH);
+          delayMicroseconds(500);
+          break;
+        case 1:
+          digitalWrite(2, HIGH);
+          delayMicroseconds(500);
+          digitalWrite(2, LOW);
+          delayMicroseconds(500);
+          break;
+        case 2:
+          digitalWrite(2, LOW       );
+          delayMicroseconds(500);
+          break;
+      }
+
+    }
+    delay(1000);
+  }
+
+}
+
+/****************************************************************/
+
+void loop()
+{
+  heatingOn();
+  delay(20000);
+
+  heatingOff();
+  delay(20000);
+}
